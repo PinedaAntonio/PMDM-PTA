261d3cc35055e78688f5f42c5bfe495c845cf394
PinedaAntonio <antonio.pinedatoro@iesvalleinclan.es>
2024-11-28 11:51:59 +0100

    hacer 7.4 y 7.5

---
+++ Tema7/Act7-4/app/src/main/java/com/example/act7_4/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.act7_4;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("ACTIVIDAD DE DIÁLOGO");
+            builder.setTitle("Toca un botón");
+            builder.setIcon(R.mipmap.ic_launcher);
+        builder.setPositiveButton("PRIMERO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast toast = Toast.makeText(getApplicationContext(), "SE HA PULSADO PRIMERO", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        builder.setNegativeButton("SEGUNDO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.setNeutralButton("TERCERO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        AlertDialog dialogo = builder.create();
+        dialogo.show();
+    }
+
+}
